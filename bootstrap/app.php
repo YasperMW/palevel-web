a52c778f2298569ad424fab40fe0f26a6e907744
YasperMW <yankhochisale4@gmail.com>
2026-02-08 13:43:47 +0200

    Update app.php
    added trustproxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'auth.palevel' => \App\Http\Middleware\AuthenticateWithPalevel::class,
             'user.profile' => \App\Http\Middleware\UserProfileMiddleware::class,
